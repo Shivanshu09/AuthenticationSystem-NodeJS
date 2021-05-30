a116fecc9c389a009ce4fc407e93a6196a17c077
Shivanshu Ahuja <shivanshuahuja@gmail.com>
2021-05-30 16:06:00 +0530

    Complete Authentication System

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const mongooose = require('mongoose');
 const path = require('path');
 const config = require('config');
 
+const authRoutes = require('./routes/authRoute');
+
 const app = express();
 app.use(express.json());
 
+app.use('/auth', authRoutes);
+
 const PORT = process.env.PORT || 3001;
+
+const url = config.get('dbURI');
+mongooose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then((result) => app.listen(PORT))
+    .catch((err) => console.log(err));
